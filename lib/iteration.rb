c846ffa25c889207fc3c73bd5fdd55e576d73f96
Asif Kamal <asif_kamal@hotmail.com>
2019-12-06 00:59:51 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -22,9 +22,8 @@ def find_greater_pair(src)
 end
 
 def total_even_pairs(src)
-  new_array = []
-  row_index = 0
   total = 0
+  row_index = 0
   while row_index < src.count do
   if (src[row_index][0] + src[row_index][1])%2 == 0
     total += (src[row_index][0] + src[row_index][1])
